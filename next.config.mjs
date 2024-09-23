7720d714270248fda39985a926d1c69b915fc414
Sureet <sureetghuman17@gmail.com>
2024-09-23 14:29:21 -0500

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,18 @@
+
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+      {
+        protocol: "https",
+        hostname: "pgcqtbmgwqrfinkqaexe.supabase.co",
+      },
+    ],
+  },
+  // Your Next.js configuration options here
+};
+
+export default nextConfig;
